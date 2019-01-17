0223f4d78217d525109ac3d97634a0229021d321
jokerx001 <jokerx001@outlook.com>
2019-01-17 23:08:45 +0800

    coding

--- src/main/java/com/homeai/usermanage/controller/UserManageController.java
+++ src/main/java/com/homeai/usermanage/controller/UserManageController.java
@@ -1,17 +1,27 @@
 package com.homeai.usermanage.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.homeai.dao.entity.UserInfo;
+import com.homeai.dao.service.UserInfoService;
+
 @Controller
 @RequestMapping("/userManage/")
 public class UserManageController {
 
+    @Autowired
+    private UserInfoService userInfoService;
+
     @RequestMapping(value="/init", method=RequestMethod.GET)
     public String init(Model model) {
 
+    	List<UserInfo> userInfoList = userInfoService.searchAllUserInfo();
         return "/userManage";
     }
 }
